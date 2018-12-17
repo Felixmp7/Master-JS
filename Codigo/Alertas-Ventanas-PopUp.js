4f8f71ff33740d35b2b4f568ea2a410cc6267a44
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-17 06:24:44 -0400

    Probando alerta, ventana y prompt

--- Codigo/Alertas-Ventanas-PopUp.js
+++ Codigo/Alertas-Ventanas-PopUp.js
@@ -7,5 +7,10 @@
 
 // Confirmacion
 
-var rpta = confirm('Desea continuar?')
-console.log(rpta)
+/*var rpta = confirm('Desea continuar?')
+console.log(rpta)*/
+
+// Prompt o Ingreso de datos
+
+var rpta = prompt('Que edad tienes?', 22);
+console.log(typeof rpta);
