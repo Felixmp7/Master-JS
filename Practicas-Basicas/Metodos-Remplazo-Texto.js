9092b7fbad4f2acb836e2936dc95f71942a132ce
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-19 18:24:58 -0400

    Implementando metodos para reemplazar texto

---
+++ Practicas-Basicas/Metodos-Remplazo-Texto.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var texto = "        Bienvenidos al     curso de Master para Javascript   ";
+
+//var busqueda = texto.replace('curso', 'tutorial'); // Reemplazar texto por otro.
+//var busqueda = texto.slice(15); // Cortar string a partir de cierto caracter.
+//var busqueda = texto.split(" "); // Corta cada palabra del array y las mete en un nuevo array separadas por posicion.
+var busqueda = texto.trim(); // Elimina espacios en blanco sobrantes (Al inicio y al final)
+
+
+
+
+console.log(busqueda);
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Master-Js
 ## Modo Estricto
 	Me obliga a seguir la sintaxis JS correctamente.
-	Escribo al inicio del documento.js = "use strict"
+	Escribo al inicio del documento.js = "use strict".
+
+### Comando para retrasar solicitud de credenciales en git
+	git config --global credential.helper 'cache --timeout 999999999'
