4ec9a9b2719d3552f6e4e81f801a04dda6ba026d
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-20 07:38:51 -0400

    Adding operations with arrays

---
+++ Arrays/Multidimensionals.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var categorias = ["Accion", "Suspenso", "Comedia"];
+var peliculas = ["Infinity", "The Purge", "Dale Duro"];
+
+var cine = [categorias,peliculas];
+console.log(cine[0][2]);
---
+++ Arrays/Operaciones.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var categorias = ["Accion", "Suspenso", "Comedia"];
+var peliculas = ["Infinity", "The Purge", "Dale Duro", "Interestelar"];
+
+peliculas.push("Forrest Gump"); // push() añade elementos al final del array
+console.log(peliculas);
+
+//peliculas.pop();	// pop() elimina ultimo elemento en el array
+//console.log(peliculas);
+
+// Para eliminar elementos en especifico, puedo hacerlo de esta manera:
+
+// Quiero eliminar Dale Duro
+
+var elementWilldelete = peliculas.indexOf('Dale Duro');
+console.log(elementWilldelete)
+
+if (elementWilldelete > -1) {
+	peliculas.splice(elementWilldelete,1); // splice elimina elementos
+}
+console.log(peliculas);
+
+var string = peliculas.join(); // convierte arrays en strings
+
+console.log(string)
