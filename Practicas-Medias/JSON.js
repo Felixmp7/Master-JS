692ce9706138ef0b25d3c556607d39a148034140
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-24 14:26:25 -0400

    JSON objects

---
+++ Practicas-Medias/JSON.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Peliculas = [
+	{
+		Ironman: [
+			{	
+				Actores:["A1","A2","A3","A4","A5","A6","A7"]
+			},
+			{
+				Año: "2007"
+			},
+			{
+				Productores: ["P1","P2","P3","P4","P5","P6","P7"]
+			},
+			{
+				Gananacias: "300"
+			}
+		]
+	},
+	{
+		Ironman2: [
+			{	
+				Actores:["A1","A2","A3","A4"]
+			},
+			{
+				Año: "2008"
+			},
+			{
+				Productores: ["P1","P2","P3","P4"]
+			},
+			{
+				Gananacias: "500"
+			}
+		]
+	},
+]
+
+console.log(Peliculas);
