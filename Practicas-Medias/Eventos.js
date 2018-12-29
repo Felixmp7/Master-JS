af7796ca0c3e7ec214453cbefc1c0aa8a4597d7f
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-29 13:36:28 -0400

    Implementing event load

--- Practicas-Medias/Eventos.js
+++ Practicas-Medias/Eventos.js
@@ -1,50 +1,51 @@
 'use strict'
 
-
-function cambiarColor(){
-	boton.style.backgroundColor = 'red';
-}
-
-var boton = document.querySelector("#boton");
+window.addEventListener('load', ()=> {
+	function cambiarColor(){
+		boton.style.backgroundColor = 'red';
+	}
+	var boton = document.querySelector("#boton");
 
 // Click
-boton.addEventListener('click', function(){
-	cambiarColor();
-});
+	boton.addEventListener('click', function(){
+		cambiarColor();
+	});
 
 // Mouse over
-boton.addEventListener('mouseover', function(){
-	boton.style.backgroundColor = 'yellow';
-});
+	boton.addEventListener('mouseover', function(){
+		boton.style.backgroundColor = 'yellow';
+	});
 
 // Mouse out
-boton.addEventListener('mouseout', function(){
-	boton.style.backgroundColor = 'black';
-});
+	boton.addEventListener('mouseout', function(){
+		boton.style.backgroundColor = 'black';
+	});
 
 
 //Focus
-var input = document.querySelector("#campo_nombre");
-input.addEventListener('focus', ()=>{
-	console.log('[Focus] estas dentro del input');
-})
+	var input = document.querySelector("#campo_nombre");
+	input.addEventListener('focus', ()=>{
+		console.log('[Focus] estas dentro del input');
+	})
 
 // Blur
-input.addEventListener('blur', ()=>{
-	console.log('[Blur] estas fuera del input');
-})
+	input.addEventListener('blur', ()=>{
+		console.log('[Blur] estas fuera del input');
+	})
 
 //Keydown
-input.addEventListener('keydown', e =>{
-	console.log('Pulsando la tecla ', String.fromCharCode(e.keyCode));
-})
+	input.addEventListener('keydown', e =>{
+		console.log('Pulsando la tecla ', String.fromCharCode(e.keyCode));
+	})
 
 //Keypress
-input.addEventListener('keypress', e =>{
-	console.log('Tecla pulsada ', String.fromCharCode(e.keyCode));
-})
+	input.addEventListener('keypress', e =>{
+		console.log('Tecla pulsada ', String.fromCharCode(e.keyCode));
+	})
 
 //Keyup
-input.addEventListener('keyup', e =>{
-	console.log('Tecla soltada ', String.fromCharCode(e.keyCode));
-})
+	input.addEventListener('keyup', e =>{
+		console.log('Tecla soltada ', String.fromCharCode(e.keyCode));
+	})
+
+});
