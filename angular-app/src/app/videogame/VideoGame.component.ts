c7997686954e434364d0b6f488e2da51a0b0b872
Felix Pacheco <felix@servisenior.com>
2021-04-07 09:37:10 -0400

    Implementa primer componente

---
+++ angular-app/src/app/videogame/VideoGame.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'videogame',
+    templateUrl: './Videogame.component.html',
+})
+export class VideoGame {
+    public title: string = 'Videojuegos';
+}
