772ec7032afc47fe0ee551c2ec59453120ece395
Felix Pacheco <felix@servisenior.com>
2021-04-08 18:06:03 -0400

    Practica implementacion de rutas

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoGame } from './videogame/videogame.component';
+import { ZapatillaComponent } from './zapatilla/zapatilla.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'zapatilla', component: ZapatillaComponent },
+  { path: 'videoGame', component: VideoGame },
+  { path: '**', component: VideoGame },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
