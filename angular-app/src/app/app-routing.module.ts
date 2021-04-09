e743c26ff567ba4a230893043443522c8609799e
Felix Pacheco <felix@servisenior.com>
2021-04-09 09:48:22 -0400

    Ignorar

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExternalComponent } from './external/external.component';
 import { VideoGame } from './videogame/videogame.component';
 import { ZapatillaComponent } from './zapatilla/zapatilla.component';
 
 const routes: Routes = [
   { path: 'zapatilla', component: ZapatillaComponent },
   { path: 'videoGame', component: VideoGame },
+  { path: 'external', component: ExternalComponent },
   { path: '**', component: VideoGame },
 ];
 
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoGame } from './videogame/videogame.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { ZapatillaComponent } from './zapatilla/zapatilla.component';
 import { FormsModule } from '@angular/forms';
+import { ExternalComponent } from './external/external.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoGame,
     CursosComponent,
-    ZapatillaComponent
+    ZapatillaComponent,
+    ExternalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
