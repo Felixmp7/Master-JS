eff3d86c8ad3f9f0fb68a7ee0ccbc1710b2853e6
Felix Pacheco <felix@servisenior.com>
2021-04-10 12:44:04 -0400

    Arranca servidor luego de conectar con la DB de Mongo

--- node-js/index.js
+++ node-js/index.js
@@ -1,11 +1,17 @@
 'use strict'
 
 const mongoose = require('mongoose');
+const app = require('./app');
+const port = 3700;
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/portafolio')
-    .then((response) => {
+    .then(() => {
         console.log('Ha cargado al DB');
+        // Lanzar servidor
+        app.listen(port, () => {
+            console.log('Servidor corriendo correctamente en localhost:3700')
+        })
     })
     .catch((error) => console.log(error))
