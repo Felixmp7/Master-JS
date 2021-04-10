cc8ac892ec407af063d539a4ab6853bb6c385a57
Felix Pacheco <felix@servisenior.com>
2021-04-10 18:02:13 -0400

    Agrega nueva ruta para obtener todos los proyectos

--- node-js/routes/project.js
+++ node-js/routes/project.js
@@ -9,5 +9,6 @@ router.get('/home', controller.home);
 router.post('/test', controller.test);
 router.post('/add-project', controller.addProject);
 router.post('/project/:id?', controller.getProject);
+router.get('/projects', controller.getAllProjects);
 
 module.exports = router;
