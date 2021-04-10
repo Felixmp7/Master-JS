d21e3bec65bdb81ee113a1fe3ef3fb39f8ae4046
Felix Pacheco <felix@servisenior.com>
2021-04-10 14:31:14 -0400

    Crea metodo para almacenar un nuevo documento en BD

--- node-js/controllers/project.js
+++ node-js/controllers/project.js
@@ -1,6 +1,8 @@
 // Será un objeto JSON o una clase que podra tener una serie de atributos y metodos con los que podra interactuar con la entidad de Proyectos.
 'use strict'
 
+const ProjectModel = require('../models/project');
+
 const controller = {
     home: (req, res) => {
         return res.status(200).send({
@@ -10,6 +12,36 @@ const controller = {
     test: (req, res) => {
         return res.status(200).send({
             status: 'success'
+        });
+    },
+    addProject: (req, res) => {
+        const newProject = new ProjectModel();
+        const { name, description, year, category, langs } = req.body;
+
+        newProject.name = name;
+        newProject.description = description;
+        newProject.year = year;
+        newProject.image = null;
+        newProject.category = category;
+        newProject.langs = langs;
+
+        newProject.save((error, projectSaved) => {
+            if (error) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: "Ocurrió un error en el servidor."
+                });
+            } else if (!projectSaved) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: "Ocurrió un error al almacenar el nuevo proyecto."
+                });
+            } else {
+                return res.status(200).send({
+                    status: 'success',
+                    data: { ...projectSaved._doc}
+                });
+            }
         });
     }
 };
