fe1703055bb9358a87f14a167ff1b7b2e010ca37
Felix Pacheco <felix@servisenior.com>
2021-04-10 13:11:27 -0400

    Practicando con Query, Params y Body en el request

--- node-js/app.js
+++ node-js/app.js
@@ -18,15 +18,39 @@ app.use((json()));
 
 app.get('/', (request, response) => {
     response.status(200).send(
-        "<h1>Root Page</h1>"
+        `<h1>Root page</h1>`
     )
-})
+});
+
+app.get('/:id', (request, response) => {
+    const id = request.params.id;
+    const query = request.query;
+    response.status(200).send(
+        `
+            <h1>
+                Hola usuario numero: ${id}
+            </h1>
+            ${query.phone ? (
+                `<b>${query.phone}</b>`
+            ) : null}
+        `
+    )
+});
+
+app.post('/', (request, response) => {
+    const { name, lastName } = request.body;
+
+    response.status(200).send(
+        `<h1>Hola ${name} ${lastName}</h1>`
+    )
+});
+
 
 app.get('/test', (request, response) => {
     response.status(200).send({
         message: 'Hola Mundo desde mi API en Express'
     })
-})
+});
 
 // exportar
 
