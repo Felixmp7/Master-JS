41a21a155d0d3dcab54865be3d19ec707cd25652
Felix Pacheco <felix@servisenior.com>
2021-04-11 11:28:39 -0400

    Agrega nueva ruta y configura middleware para permitir recepción de files

--- node-js/routes/project.js
+++ node-js/routes/project.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const express = require('express');
+const multiParty = require('connect-multiparty');
 const controller = require('../controllers/project');
 
+const multyPartyMiddleware = multiParty({ uploadDir: './uploads'})
 const router = express.Router();
 
 router.get('/home', controller.home);
@@ -12,5 +14,6 @@ router.post('/project/:id?', controller.getProject);
 router.get('/projects', controller.getAllProjects);
 router.put('/project/:id', controller.updateProject);
 router.delete('/project/:id', controller.deleteProject);
+router.post('/upload-image/:id', multyPartyMiddleware, controller.uploadImage);
 
 module.exports = router;
